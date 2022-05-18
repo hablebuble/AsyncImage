4d0cc3464ff9a1b169eabc99cb748ef7b5e21f3a
Alexey Strelnikov <strelkov@me.com>
2022-05-18 13:10:02 +0300

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Alexey Strelnikov on 18.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
