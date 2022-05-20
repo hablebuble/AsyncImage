62d39e7fe46381eb844b3930c73151643422332d
Alexey Strelnikov <strelkov@me.com>
2022-05-20 18:02:26 +0300

    Finished AsyncImage Article

--- AsyncImage/ContentView.swift
+++ AsyncImage/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    let imageURL = "https://images.unsplash.com/photo-1652986708172-935f37f52fef?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: .spring())) { phase in
+            switch phase {
+            case .empty:
+                Color.purple.opacity(0.1)
+         
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFill()
+         
+            case .failure(_):
+                Image(systemName: "exclamationmark.icloud")
+                    .resizable()
+                    .scaledToFit()
+         
+            @unknown default:
+                Image(systemName: "exclamationmark.icloud")
+            }
+        }
+        .frame(width: 300, height: 500)
+        .cornerRadius(20)
+
     }
 }
 
